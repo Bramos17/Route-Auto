c52e0ab5146f825e2c4594ed17e8d50296282ea8
Brandon Ramos <Brandon-ramos17@outlook.com>
2023-06-06 16:56:21 -0600

    Brandon app.js

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,35 +1,30 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
-// Sales imports
+
+
 import RecordList from './sales/RecordList';
 import RecordHistory from './sales/RecordHistory';
 import CustomerForm from './sales/CustomerForm';
-import EmployeeForm from './sales/EmployeeForm';
+import EmployeeForm from './sales/StaffForm';
 import RecordForm from './sales/RecordForm';
-
-
 
 function App() {
   return (
     <BrowserRouter>
-      {/* Navigation */}
       <Nav />
-      {/* Main content */}
       <div className="container">
         <Routes>
-          {/* Home page */}
           <Route path="/" element={<MainPage />} />
-          {/* Sales Routes */}
-          <Route path="/sales" element={<RecordList />} />
-          <Route path="/sales_history" element={<RecordHistory />} />
+          <Route path="/records" element={<RecordList />} />
+          <Route path="/record_history" element={<RecordHistory />} />
           <Route path="/new_customer" element={<CustomerForm />} />
           <Route path="/new_sales_person" element={<EmployeeForm />} />
-          <Route path="/new_sale" element={<RecordForm />} />
+          <Route path="/new_record" element={<RecordForm />} />
         </Routes>
       </div>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
