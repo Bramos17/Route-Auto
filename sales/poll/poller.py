3e310b1b4fd8b4f30517571ba7ec29c737ca1c9d
Brandon Ramos <Brandon-ramos17@outlook.com>
2023-06-06 08:35:13 -0600

    Poller

--- sales/poll/poller.py
+++ sales/poll/poller.py
@@ -10,7 +10,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sales_project.settings")
 django.setup()
 
 # Import models from sales_rest, here.
-# from sales_rest.models import Something
 from sales_rest.models import AutomobileVO
 
 
@@ -18,20 +17,15 @@ def poll(repeat=True):
     while True:
         print('Sales poller polling for data')
         try:
-            response = requests.get(
-                "http://inventory-api:8000/api/automobiles/"
-                )
+            response = requests.get("http://inventory-api:8000/api/automobiles/")
             content = json.loads(response.content)
             for automobile in content:
                 AutomobileVO.objects.update_or_create(
                     import_href=automobile['import_href'],
-                    defaults={
-                        'vin': automobile['vin'],
-                    }
+                    defaults={'vin': automobile['vin']},
                 )
                 print(automobile)
 
-            pass
         except Exception as e:
             print(e, file=sys.stderr)
 
