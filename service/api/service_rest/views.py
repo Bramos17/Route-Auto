f3164066616920411fc73ec6ca978f0111e444d0
Sophia Tony-Egbuniwe <ost2105@columbia.edu>
2023-06-06 09:38:20 -0500

    added 400 errors to view functions for each endpoint if they are unsuccessful or accessing models that don't exist

--- service/api/service_rest/views.py
+++ service/api/service_rest/views.py
@@ -48,48 +48,78 @@ class AppointmentDetailEncoder(ModelEncoder):
     }
 
 
-@require_http_methods(["GET", "POST", "DELETE"])
+@require_http_methods(["GET", "POST"])
 def api_list_technicians(request):
     if request.method == "GET":
-        technicians = Technician.objects.all()
-        return JsonResponse(
-            {'technicians': technicians},
-            encoder=TechnicianListEncoder
-        )
+        try:
+            technicians = Technician.objects.all()
+            return JsonResponse(
+                {'technicians': technicians},
+                encoder=TechnicianListEncoder
+            )
+        except Technician.DoesNotExist:
+            return JsonResponse(
+                {'message': 'No technicians to list'},
+                status=400,
+            )
     else:
-        content = json.loads(request.body)
-        technician = Technician.objects.create(**content)
-        return JsonResponse(
-            technician,
-            encoder=TechnicianDetailEncoder,
-            safe=False,
-        )
+        try:
+            content = json.loads(request.body)
+            technician = Technician.objects.create(**content)
+            return JsonResponse(
+                technician,
+                encoder=TechnicianDetailEncoder,
+                safe=False,
+            )
+        except TypeError:
+            return JsonResponse(
+                {'message': 'Invalid key name'},
+                status=400,
+            )
 
 
 @require_http_methods(["GET", "DELETE"])
 def api_show_technician(request, id):
     if request.method == "GET":
-        technician = Technician.objects.get(id=id)
-        return JsonResponse(
-            technician,
-            encoder=TechnicianDetailEncoder,
-            safe=False,
-        )
-    elif request.method == "DELETE":
-        count, _ = Technician.objects.filter(id=id).delete()
-        return JsonResponse(
-            {'deleted': count > 0}
-        )
+        try:
+            technician = Technician.objects.get(id=id)
+            return JsonResponse(
+                technician,
+                encoder=TechnicianDetailEncoder,
+                safe=False,
+            )
+        except Technician.DoesNotExist:
+            return JsonResponse(
+                {'message': 'Technician id is invalid'},
+                status=400,
+            )
+    else:
+        try:
+            count, _ = Technician.objects.filter(id=id).delete()
+            return JsonResponse(
+                {'deleted': count > 0}
+            )
+        except Technician.DoesNotExist:
+            return JsonResponse(
+                {'message': 'Technician has already been deleted'},
+                status=400,
+            )
 
 
 @require_http_methods(["GET", "POST"])
 def api_list_appointment(request):
     if request.method == "GET":
-        appointments = Appointment.objects.all()
-        return JsonResponse(
-            {'appointments': appointments},
-            encoder=AppointmentListEncoder,
-        )
+        try:
+            appointments = Appointment.objects.all()
+            return JsonResponse(
+                {'appointments': appointments},
+                encoder=AppointmentListEncoder,
+            )
+        except Appointment.DoesNotExist:
+            JsonResponse(
+                {'message': 'No appointments to list'},
+                status=400,
+            )
     else:
         content = json.loads(request.body)
         try:
@@ -111,17 +141,29 @@ def api_list_appointment(request):
 @require_http_methods(["GET", "DELETE"])
 def api_show_appointment(request, id):
     if request.method == "GET":
-        appointment = Appointment.objects.get(id=id)
-        return JsonResponse(
-            appointment,
-            encoder=AppointmentDetailEncoder,
-            safe=False,
-        )
-    elif request.method == "DELETE":
-        count, _ = Appointment.objects.filter(id=id).delete()
-        return JsonResponse(
-            {'deleted': count > 0},
-        )
+        try:
+            appointment = Appointment.objects.get(id=id)
+            return JsonResponse(
+                appointment,
+                encoder=AppointmentDetailEncoder,
+                safe=False,
+            )
+        except Appointment.DoesNotExist:
+            return JsonResponse(
+                {'message': 'Invalid appointment id'},
+                status=400,
+            )
+    else:
+        try:
+            count, _ = Appointment.objects.filter(id=id).delete()
+            return JsonResponse(
+                {'deleted': count > 0},
+            )
+        except Appointment.DoesNotExist:
+            return JsonResponse(
+                {'message': 'Appointment has already been deleted'},
+                status=400,
+            )
 
 
 @require_http_methods(["PUT"])
