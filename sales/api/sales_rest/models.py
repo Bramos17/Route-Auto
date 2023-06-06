9bc8b10cf72251127c9c211f960838b085d158bc
Brandon Ramos <Brandon-ramos17@outlook.com>
2023-06-06 10:03:23 -0600

    phone number fix

--- sales/api/sales_rest/models.py
+++ sales/api/sales_rest/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.urls import reverse
-from phonenumber_field.modelfields import PhoneNumberField
 
 
 class AutomobileVO(models.Model):
@@ -28,7 +27,7 @@ class Customer(models.Model):
     last_name = models.CharField(max_length=50)
     address = models.CharField(max_length=50)
     email = models.EmailField()
-    phone_number = PhoneNumberField(blank=True)
+    phone_number = models.CharField(max_length=15)
 
     def __str__(self):
         return self.first_name + " " + self.last_name
